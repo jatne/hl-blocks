2f0d6a3e1fb57f124eb10e6b5525b1db7f9a8e41
jatne <pawelmalok@gmail.com>
2021-10-28 12:24:19 +0200

    Update schema.js

--- packages/studio/schemas/schema.js
+++ packages/studio/schemas/schema.js
@@ -1,16 +1,29 @@
-// First, we must import the schema creator
 import createSchema from 'part:@sanity/base/schema-creator'
-
-// Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
-// Then we give our schema to the builder and provide the result to Sanity
+// Docs
+import page from './documents/page'
+import post from './documents/post'
+import author from './documents/author'
+import siteSettings from './documents/siteSettings'
+import navMenu from './documents/navMenu'
+
+// Components
+import emptySpace from './components/emptySpace'
+import richEditor from './components/richEditor'
+
 export default createSchema({
-  // We name our schema
   name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    // docs:
+    page,
+    post,
+    author,
+    siteSettings,
+    navMenu,
+
+    // components:
+    emptySpace,
+    richEditor,
   ]),
 })
