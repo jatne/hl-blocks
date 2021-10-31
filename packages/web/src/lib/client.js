a97404376bb9d1d46411c59bcbf301a464ca900c
jatne <pawelmalok@gmail.com>
2021-10-31 22:03:44 +0100

    Updated API Version

--- packages/web/src/lib/client.js
+++ packages/web/src/lib/client.js
@@ -3,7 +3,7 @@ import sanityClient from '@sanity/client'
 const client = sanityClient({
   projectId:  import.meta.env.VITE_SANITY_PROJECT_ID,
   dataset: import.meta.env.VITE_SANITY_PROJECT_DATASET,
-  apiVersion: '2021-03-25',
+  apiVersion: '2021-10-26',
   token: import.meta.env.VITE_SANITY_PROJECT_TOKEN,
   useCdn: false
 })
