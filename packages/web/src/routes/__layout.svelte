0201c473afda86e400c24025b44802696812f9ae
jatne <pawelmalok@gmail.com>
2021-10-31 22:04:22 +0100

    create dummy layout

---
+++ packages/web/src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<nav>
+	<ul>
+		<li><a sveltekit:prefetch href="/">Home</a></li>
+		<li><a sveltekit:prefetch href="/about">About</a></li>
+		<li><a sveltekit:prefetch href="/contact">Contact</a></li>
+	</ul>
+</nav>
+<slot />
