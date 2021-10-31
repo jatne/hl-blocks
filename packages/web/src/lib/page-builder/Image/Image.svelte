39b35dfc5a9e9084b6bb75a3915f1c855d862a64
jatne <pawelmalok@gmail.com>
2021-10-31 22:04:09 +0100

    cleaning

--- packages/web/src/lib/page-builder/Image/Image.svelte
+++ packages/web/src/lib/page-builder/Image/Image.svelte
@@ -1,14 +1,1 @@
-<script context="module">
-	import client from '$lib/client'
-</script>
-
-<script>
-	import imageUrlBuilder from '@sanity/image-url'
-	export let asset
-
-	function urlFor(source) {
-		return imageUrlBuilder(client).image(source)
-	}
-</script>
-
-<div class="image"><img src={urlFor(asset)} alt /></div>
+<div class="image" />
