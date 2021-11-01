c87223450d825ec12695ec7a84806e457e06830b
jatne <pawelmalok@gmail.com>
2021-11-01 21:12:48 +0100

    navMenu schema

--- packages/studio/schemas/documents/navMenu.js
+++ packages/studio/schemas/documents/navMenu.js
@@ -11,5 +11,45 @@ export default {
       name: 'name',
       type: 'string',
     },
-  ]
+    {
+      type: 'array',
+      name: 'items',
+      of: [{
+        title: "Link",
+        name: "link",
+        type: 'object',
+        fields: [
+          {
+            title: 'Link Label',
+            name: 'label',
+            type: 'string',
+          },
+          {
+            title: "Select link type",
+            name: 'linkType',
+            type: 'string',
+            options: {
+              list: [
+                {title: 'Internal', value: 'internal'},
+                {title: 'External', value: 'external'}
+              ],
+            }
+          },
+          {
+            title: 'Page (internal)',
+            name: 'linkInternal',
+            type: 'reference',
+            to: [{type: 'page'}],
+            hidden: ({ parent }) => parent?.linkType === undefined || parent?.linkType === 'external'
+          },
+          {
+            title: 'Page (external)',
+            name: 'linkExternal',
+            type: 'url',
+            hidden: ({ parent }) => parent?.linkType === undefined || parent?.linkType === 'internal'
+          },
+        ]
+      }]
+    }
+  ],
 }
