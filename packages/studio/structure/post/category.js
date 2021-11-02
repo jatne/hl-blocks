14e9323e5bee416ec28121b702644e92438a5d9e
jatne <pawelmalok@gmail.com>
2021-11-02 14:57:21 +0100

    schema for category

--- packages/studio/structure/post/category.js
+++ packages/studio/structure/post/category.js
@@ -4,3 +4,10 @@ import {AiFillTags} from 'react-icons/ai';
 export default S.listItem()
   .title('Category')
   .icon(AiFillTags)
+  .schemaType('author')
+  .child(
+    S.documentList('category')
+      .title('Category')
+      .menuItems(S.documentTypeList('category').getMenuItems())
+      .filter('_type == "category"')
+  )
