10d04677d196d6d153aa53df51ff8c68ef333bf3
jatne <pawelmalok@gmail.com>
2021-12-14 12:56:36 +0100

    update eslint

--- packages/web/src/lib/client.js
+++ packages/web/src/lib/client.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client'
 
 const client = sanityClient({
-  projectId:  import.meta.env.VITE_SANITY_PROJECT_ID,
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
   dataset: import.meta.env.VITE_SANITY_PROJECT_DATASET,
   apiVersion: '2021-10-26',
   token: import.meta.env.VITE_SANITY_PROJECT_TOKEN,
