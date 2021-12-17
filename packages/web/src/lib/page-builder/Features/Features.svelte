959d2ae24a53d12b47b4f6d9af8fad499af7846f
jatne <pawelmalok@gmail.com>
2021-12-17 12:48:47 +0100

    component: features

---
+++ packages/web/src/lib/page-builder/Features/Features.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Feature from './Feature.svelte'
+	export let content
+</script>
+
+{#if content.length > 0}
+	<dl
+		class="mt-12 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 lg:grid-cols-4 lg:gap-x-8r"
+	>
+		{#each content as { title, description }, i}
+			<Feature {title} {description} />
+		{/each}
+	</dl>
+{/if}
