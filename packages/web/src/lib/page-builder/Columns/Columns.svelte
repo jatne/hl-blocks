2d2147d6954ab06d0ffc0794defaaf7d0882b24a
jatne <pawelmalok@gmail.com>
2022-01-13 21:29:26 +0100

    component: columns

---
+++ packages/web/src/lib/page-builder/Columns/Columns.svelte
@@ -0,0 +1,37 @@
+<script>
+	import PageBuilder from '$lib/page-builder/PageBuilder.svelte'
+	export let style
+	export let content
+
+	const [colsNumber, colsStyle] = style.split('_')
+
+	const columns = Object.values(content).filter((col) => col)
+</script>
+
+<div class={`wrapper wrapper--${colsNumber} wrapper--${colsStyle}`}>
+	{#each columns as column}
+		<div class="column">
+			<PageBuilder blocks={column} />
+		</div>
+	{/each}
+</div>
+
+<style>
+	.wrapper {
+		display: grid;
+		grid-template-columns: var(--columns, 1fr);
+		gap: 1.5em;
+	}
+
+	.wrapper--2 {
+		--columns: 1fr 1fr;
+	}
+
+	.wrapper--3 {
+		--columns: repeat(3, 1fr);
+	}
+
+	.wrapper--40-60 {
+		--columns: 0.4fr 0.6fr;
+	}
+</style>
